2f7222629df5dcfa5672f5ea7000bf5e8e42cd9e
Jonathan Fereira <jonathanfereira11@gmail.com>
2023-04-10 20:16:48 -0400

    añadido codigo, cambio a README y .gitignore

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ Actividad mini taller Git y Github
     Aprendiendo las bases de como funciona un repositorio de Git y Github.
 
 Me llamo **Jonathan Fereira**
+
+No se como se crea el archivo tempCodeRunnerFile.py solo lo añadí en la primera entrada para que se note el cambio en el segundo commit
--- py_project.py
+++ py_project.py
@@ -1,1 +1,24 @@
-#Nada por ahora, espere a que se me ocurra algo :)
+#Cualquier parecido con otro codigo es pura y mera coincidencia :)
+
+import time
+
+print("Este programa calcula la cantidad de primos entre 1 y n\n")
+
+while True:
+    n = int(input("Ingresa un valor para n: "))
+
+    inicio = time.time()
+
+    contador = 0
+    for num in range(2, n + 1):
+        es_primo = True
+        for div in range(2, num):
+            if num % div == 0:
+                es_primo = False
+        if es_primo:
+            contador += 1
+
+    fin = time.time()
+
+    print("Hay", contador, "numeros primos entre 1 y", n)
+    print("El tiempo de ejecución fue de", fin - inicio, "seg\n")
